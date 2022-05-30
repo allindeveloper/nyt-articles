42cdc8fcd219d2d7dbdb41c847a70fb8f1573074
Uchendu Precious <uchendubozz@gmail.com>
2022-05-30 12:51:09 +0100

    added global constants and extra utility

---
+++ src/constants/globalConstants.js
@@ -0,0 +1,3 @@
+export const api = {
+    ARTICLE_SEARCH : "articlesearch",
+}
--- src/utils/globalUtility.js
+++ src/utils/globalUtility.js
@@ -1,3 +1,6 @@
 export const getApiKey = () =>{
     return process.env.REACT_APP_API_KEY;
 }
+export const getBaseUrl = () => {
+    return process.env.REACT_APP_BASE_URL;
+}
