f958e7de2c6237a3db9d015b6be2809a0b9f5039
Uchendu Precious <uchendubozz@gmail.com>
2022-05-30 12:58:47 +0100

    create and hooked up our custom theming

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { myTheme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={myTheme} >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
